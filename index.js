4df1866401d25633b7422a93888a56652e8d31f3
Minh Quan <buiminhquan0307@gmail.com>
2025-06-20 08:57:06 +0700

    portalv10

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 // It uses process.env.PORT for deployment environments or defaults to 3000 for local development.
 const port = process.env.PORT || 3000;
 
+
+app.use(express.static('public'));
 // Define a route for the root URL ('/')
 // When a GET request is made to '/', the callback function will be executed.
 app.get('/', (req, res) => {
@@ -16,7 +18,7 @@ app.get('/', (req, res) => {
   // __dirname refers to the directory where the current script (app.js) is located.
   // 'public' is the folder containing our HTML file.
   // 'index.html' is the name of the HTML file we want to send.
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile('index.html');
 });
 
 // Start the server and listen for incoming requests on the specified port
